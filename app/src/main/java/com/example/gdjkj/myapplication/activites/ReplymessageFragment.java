7a5f8e9850219c9e91a6829badf9b8c259fa12ea
ajay.maheshwari@ranosys.com <gdjkjhome>
2016-11-21 15:49:50 +0530

    wedding appwith respond.

---
+++ app/src/main/java/com/example/gdjkj/myapplication/activites/ReplymessageFragment.java
@@ -0,0 +1,9 @@
+package com.example.gdjkj.myapplication.activites;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ajay on 21/11/16.
+ */
+public class ReplymessageFragment extends Fragment {
+}
